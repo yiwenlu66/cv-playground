bf56ffdb22f94803bfed849d5e5a61709e5792e2
Peter Lu <mcfloundinho@gmail.com>
2017-01-13 17:18:54 +0800

    video: minor fix

--- video/analyze.py
+++ video/analyze.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Author: Song Shihong
 
 import cv2
+import sys
 
-videoCapture = cv2.VideoCapture('1.mov')
+videoCapture = cv2.VideoCapture(sys.argv[1])
 
 #获得码率及尺寸
 fps = videoCapture.get(cv2.cv.CV_CAP_PROP_FPS)
@@ -16,9 +18,7 @@ success, frame = videoCapture.read()
 c = 0
 
 while success :
-    cv2.waitKey(1000/int(fps)) #延迟
+    # cv2.waitKey(1000/int(fps)) #延迟
     cv2.imwrite('image/' + str(c) + '.jpg',frame) #写视频帧
     success, frame = videoCapture.read() #获取下一帧
-    if c > 100:
-        break
     c += 1
